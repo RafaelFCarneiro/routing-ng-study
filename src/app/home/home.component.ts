cf15e1621e38e02d3fc87a43c1081272c6751d35
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2017-10-13 13:56:21 -0300

    create fake login and logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -22,5 +23,13 @@ export class HomeComponent implements OnInit {
         fragment: 'loading'
       }
     );
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
