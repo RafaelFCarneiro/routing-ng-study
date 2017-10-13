8edc807680184bf6911dd3c42c177608ab31fd94
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2017-10-13 10:32:48 -0300

    use query params and ancor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onLoadServers(id: number){
+    // do something
+    this.router.navigate(
+      ['/servers', id, 'edit'],
+      {
+        queryParams: {allowEdit: '1'},
+        fragment: 'loading'
+      }
+    );
+  }
 }
